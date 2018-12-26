31ab339f4b30d34c0c6bed3a5cc6aeb7b252f701
skysingh1995 <singh.aakash5111995@gmail.com>
2018-12-26 11:30:26 +0530

    make it better

--- src/app/pages/starter/starter.component.ts
+++ src/app/pages/starter/starter.component.ts
@@ -3,10 +3,10 @@ import { Component, AfterViewInit } from '@angular/core';
 	templateUrl: './starter.component.html'
 })
 export class StarterComponent implements AfterViewInit {
-	subtitle:string;	
-	constructor() {
-		this.subtitle = "This is some text within a card block."
-	}
+  subtitle: string;
+    constructor() {
+      this.subtitle = "This is some text within a card block."
+    }
 
 	ngAfterViewInit(){}
 }
