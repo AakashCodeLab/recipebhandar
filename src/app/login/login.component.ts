8953ec726f5ec10c79eaf01e5dad506f96c1b81d
skysingh1995 <singh.aakash5111995@gmail.com>
2018-12-28 14:45:32 +0530

    validate user exits or not show alert bar

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -46,37 +46,41 @@ export class LoginComponent implements OnInit, AfterViewInit {
         return;
       }
       const registeruser = JSON.parse(localStorage.getItem('isLoggedin'));
-      console.log(registeruser)
+      console.log(registeruser);
 
-     // const  self = this;
-     const verified = registeruser.some( (obj) => {
-       if ( (obj.userName === this.loginForm.value.userName) === false ) {
-         if ( (obj.password ===  this.loginForm.value.password) === false) {
-           this.errorvalue = 3; // both wrong
-           return false;
-         }else {
-           this.errorvalue = 1; // username wrong
-           return false;
-         }
-       }else
-         if ((obj.password ===  this.loginForm.value.password) === true) {
-           this.errorvalue = 0;   // nothing wrong
-           return true;
-       } else {
-           this.errorvalue = 2;  // password wrong
-           return true;
-         }
-      })
+      if (registeruser) {
+        // const  self = this;
+        const verified = registeruser.some( (obj) => {
+          if ( (obj.userName === this.loginForm.value.userName) === false ) {
+            if ( (obj.password ===  this.loginForm.value.password) === false) {
+              this.errorvalue = 3; // both wrong
+              return false;
+            }else {
+              this.errorvalue = 1; // username wrong
+              return false;
+            }
+          }else
+          if ((obj.password ===  this.loginForm.value.password) === true) {
+            this.errorvalue = 0;   // nothing wrong
+            return true;
+          } else {
+            this.errorvalue = 2;  // password wrong
+            return true;
+          }
+        })
 
-      if ( !this.errorvalue) {
-       console.log(this.errorvalue);
-       this.router.navigate(['/home']);
-      } else if (this.errorvalue === 1) {
-        console.log('username wrong');
-      }else if (this.errorvalue === 2) {
-        console.log('password wrong');
-      } else if (this.errorvalue === 3) {
-        console.log('both wrong');
+        if ( !this.errorvalue) {
+          console.log(this.errorvalue);
+          this.router.navigate(['/home']);
+        } else if (this.errorvalue === 1) {
+          console.log('username wrong');
+        }else if (this.errorvalue === 2) {
+          console.log('password wrong');
+        } else if (this.errorvalue === 3) {
+          console.log('both wrong');
+        }
+      }else {
+        this.errorvalue = 3;
       }
     }
   clearFilter() {
