54518b656c58dacfc4523e1ac243d19342df68b5
skysingh1995 <singh.aakash5111995@gmail.com>
2018-12-28 12:40:08 +0530

    validation to check what is wrong and right for login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -13,6 +13,7 @@ import { Router } from '@angular/router';
 export class LoginComponent implements OnInit, AfterViewInit {
   loginForm: FormGroup;
   submitted = false;
+  errorvalue;
     constructor(public router: Router, private formBuilder: FormBuilder) {}
 
     ngOnInit() {
@@ -46,16 +47,39 @@ export class LoginComponent implements OnInit, AfterViewInit {
       }
       const registeruser = JSON.parse(localStorage.getItem('isLoggedin'));
       console.log(registeruser)
-      const  self = this
-     const verified = registeruser.some(function (obj) {
-        return obj.userName === self.loginForm.value.userName &&  obj.password ===  self.loginForm.value.password ;
+
+     // const  self = this;
+     const verified = registeruser.some( (obj) => {
+       if ( (obj.userName === this.loginForm.value.userName) === false ) {
+         if ( (obj.password ===  this.loginForm.value.password) === false) {
+           this.errorvalue = 3; // both wrong
+           return false;
+         }else {
+           this.errorvalue = 1; // username wrong
+           return false;
+         }
+       }else
+         if ((obj.password ===  this.loginForm.value.password) === true) {
+           this.errorvalue = 0;   // nothing wrong
+           return true;
+       } else {
+           this.errorvalue = 2;  // password wrong
+           return true;
+         }
       })
 
-      if (verified) {
-        this.router.navigate(['/home']);
-      }else {
-        return false;
+      if ( !this.errorvalue) {
+       console.log(this.errorvalue);
+       this.router.navigate(['/home']);
+      } else if (this.errorvalue === 1) {
+        console.log('username wrong');
+      }else if (this.errorvalue === 2) {
+        console.log('password wrong');
+      } else if (this.errorvalue === 3) {
+        console.log('both wrong');
       }
     }
-
+  clearFilter() {
+    this.errorvalue = 0;  // clear value when input value change
+  }
 }
