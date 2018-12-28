7fd0b94bdcb69ce62176e68cdd5e7a72602a5316
skysingh1995 <singh.aakash5111995@gmail.com>
2018-12-28 17:33:58 +0530

    added pwa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {SwPush, SwUpdate} from '@angular/service-worker';
+import {MatSnackBar} from '@angular/material';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  constructor(private swUpdate: SwUpdate, private swPush: SwPush, private snackbar: MatSnackBar,
+              ) {
+
+    this.swUpdate.available.subscribe(evt => {
+      this.swUpdate.checkForUpdate().then(() => {
+
+        const snack = this.snackbar.open('Update Available', 'Reload', {
+          panelClass: ['blue-snackbar']
+        });
+        snack
+          .onAction()
+          .subscribe(() => {
+            window.location.reload();
+          });
+
+      });
+    });
+
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import * as $ from 'jquery';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -8,13 +9,37 @@ import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './shared/guard/auth.guard';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {SwPush, SwUpdate} from '@angular/service-worker';
 import { LocationStrategy, PathLocationStrategy } from '@angular/common';
-
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import {
+  MatToolbarModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatListModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatSortModule,
+  MatSnackBarModule,
+  MatSnackBar
+} from '@angular/material';
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule, HttpClientModule, AppRoutingModule, ReactiveFormsModule],
+  imports: [ BrowserAnimationsModule, BrowserModule, FormsModule,
+    HttpClientModule, MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatSnackBarModule,
+    AppRoutingModule, ReactiveFormsModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
   providers: [
+    SwUpdate,
     AuthGuard,
     {
       provide: LocationStrategy,
