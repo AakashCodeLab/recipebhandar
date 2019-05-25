69481af8160db698c3462be3e32b3cd97c328b07
aakash1801 <singh.aakash1801@gmail.com>
2019-05-25 18:56:30 +0530

    change logo

--- src/app/services/recipedata.service.ts
+++ src/app/services/recipedata.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {MatSnackBar} from '@angular/material';
+import {Router} from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class RecipedataService {
   uri = 'recipe';
   versionuri = 'getversion';
 
-  constructor(private http: HttpClient,private snackbar: MatSnackBar) { }
+  constructor(private http: HttpClient,private snackbar: MatSnackBar,public router: Router,) { }
   getRecipes() {
     return this .http.get(`${this.uri}`);
   }
@@ -32,6 +33,7 @@ export class RecipedataService {
           duration: 500,
           panelClass: ['blue-snackbar']
         });
+        this.router.navigate(['/home']);
         console.log('Done');
       },(err) => {
         const snack = this.snackbar.open('Sorry ,for server error','', {
