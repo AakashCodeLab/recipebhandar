bef368f23850b57694946c851ec3c08431f9f258
saisawant <saisawant29@gmail.com>
2019-05-29 16:27:57 +0530

    remove forgot password

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -29,7 +29,72 @@ export class AuthenticationService {
       .map(response => {
         return response;
       });
-  };
+  }
+
+
+ //  /*
+ //  * Forgot Password
+ //  * */
+ //  forgotPassword(email): Observable<any> {
+ //    let url = this.uri + '/api/user/password/forgot/';
+ //
+ //    let body = {
+ //      "email": email
+ //    };
+ //    return this.httpClient.post(url, body)
+ //      .map(response => {
+ //        let resdata = response;
+ //
+ //        return resdata;
+ //      });
+ //  }
+ //
+ //  /*
+ //  * Reset Password
+ //  * */
+ //  resetPassword(body, ID): Observable<any> {
+ //    let url: string = this.uri + '/api/user/password/reset/' + ID + '/';
+ //    return this.httpClient.post(url, body)
+ //      .map(res => {
+ //        return res;
+ //      })
+ //  }
+ //
+ //
+ //  /*
+ //  * Read Reset Password
+ //  * */
+ //  readResetPassword(ID): Observable<any> {
+ //    let url: string = this.uri + '/api/user/password/reset/' + ID + '/';
+ //    return this.httpClient.get(url)
+ //      .map(res => {
+ //        return res;
+ //      })
+ //  }
+ //
+ //
+ //  /*
+ // * Reset Password
+ // * */
+ //  changePassword(iobj): Observable<any> {
+ //    let url = this.uri + '/api/user/userpassword/reset/ ';
+ //
+ //    // let obj = JSON.parse(localStorage.getItem('cmLocal'));
+ //    // let Token = obj.token.key;
+ //    //
+ //    // let headers: any = new Headers();
+ //    // headers.append('Content-Type', 'application/json');
+ //    // headers.append('Authorization', `Token ${Token}`);
+ //
+ //    // let options = new RequestOptions({ headers: headers });
+ //
+ //    return this.httpClient.post(url, iobj)
+ //      .map(response => {
+ //        let resdata = response;
+ //        return resdata;
+ //      });
+ //  }
+
 
 }
 
