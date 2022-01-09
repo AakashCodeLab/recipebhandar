1713631d7322c6da5b20b68ea14ec032fa3477d4
Aakash Singh <singh.aakash1801@gmail.com>
2022-01-09 16:43:19 +0530

    Update DB.js

--- api/DB.js
+++ api/DB.js
@@ -1,7 +1,7 @@
 
 module.exports = {
-  // DB: 'mongodb+srv://aakash1801:resonance252@cluster0-5juqi.mongodb.net/test?retryWrites=true'
-   DB: 'mongodb+srv://aakash1801:Sk5r14dpm1967@cluster0.2stnu.mongodb.net/test?retryWrites=true&w=majority'
+  DB: 'mongodb+srv://aakash1801:resonance252@cluster0-5juqi.mongodb.net/test?retryWrites=true'
+   
  };
 
 
